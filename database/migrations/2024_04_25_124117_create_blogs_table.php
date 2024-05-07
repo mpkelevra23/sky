24bc2c65d7c740f2b84b66765cb73f42974f2494
mpkelevra23 <mpkelevra23@gmal.com>
2024-05-07 14:40:55 +0400

    Add unique constraints
    In this commit, unique constraints were added to 'user_id' in the blogs table and to 'name' in both the tags and categories tables to avoid duplication.

--- database/migrations/2024_04_25_124117_create_blogs_table.php
+++ database/migrations/2024_04_25_124117_create_blogs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->unique()->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('description');
             $table->string('image')->nullable();
